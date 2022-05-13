3a9ecccabb49a840991455780ebc601033f7b754
Vadym Samusenko <vsamusenko@localhost.vsamusenkows>
2022-05-13 11:46:14 +0300

    Initial commit, v0.1

---
+++ temp.py
@@ -0,0 +1,24 @@
+
+from optparse import OptionParser
+
+def do_the_thing(out, sframe, eframe, userange):
+    rnode = hou.node(out)
+    if bool(userange):
+        rnode.render(frame_range=(sframe, eframe))
+    else:
+        rnode.render()
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-i", "--hip", dest="hipfile", help="path to .hip file")
+    parser.add_option("-o", "--out", dest="outnode", help="path to out node")
+    parser.add_option("-s", "--sframe", dest="startframe", help="start frame to render")
+    parser.add_option("-e", "--eframe", dest="endframe", help="end frame to render")
+    parser.add_option("-u", "--userange", dest="userange", help="toggle to enable frame range")
+
+
+    (options, args) = parser.parse_args()
+    
+    hou.hipFile.load(options.hipfile)
+
+    do_the_thing(options.outnode, int(options.startframe), int(options.endframe), options.userange)
